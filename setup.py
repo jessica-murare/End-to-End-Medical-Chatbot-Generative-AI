2a182af0cbc60e4b308d20975347f3f86330259e
Khushi Yadav <yadavkhushi.31.01@gmail.com>
2025-04-18 22:31:39 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author_email='yadavkhushi.31.01gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
